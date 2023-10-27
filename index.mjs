0e6f7c03db2e583fb739ded03b3b813d6eba9547
mditrich_ad <mditrich_ad@POL-6G3XVG3.SIIPOLSKA.PL>
2023-10-27 22:03:40 +0200

    CORS

--- index.mjs
+++ index.mjs
@@ -25,6 +25,11 @@ export const handler = () => {
     let jsonResponse = {"hello":"world"}
     const response = {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify(jsonResponse),
     };
     return response;
